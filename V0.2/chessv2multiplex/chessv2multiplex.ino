0e299f861e6268abf721183850c382925ecd1dec
Matthew A. Bardin <mbardin.2013@gmail.com>
2026-01-27 11:56:31 -0600

    updating Arduino code for multiplexer
    Max code is also updated, but doesn't include sound files.

--- V0.2/chessv2multiplex/chessv2multiplex.ino
+++ V0.2/chessv2multiplex/chessv2multiplex.ino
@@ -6,8 +6,8 @@
 #include <light_CD74HC4067.h>
 
 CD74HC4067 mux(8,9,10,11); //s0 s1 s2 s3
-const int signal_pin = 3
-int dTime = 100; // delay time for multiplexer
+const int signal_pin = 3;
+int dTime = 50; // delay time for multiplexer
 
 void setup() {
   // Switch Pin inputs
@@ -26,6 +26,7 @@ void loop() {
           Serial.print(i + 1);  // Print channel
           Serial.print(" ");
           Serial.print(val); // print val
+          Serial.print(" ");
           delay(dTime); // slight delay for multiplexer 
       }
       Serial.println(" "); // carriage return after all 16
